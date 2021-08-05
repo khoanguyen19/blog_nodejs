5c8d775641675326d9b2223ef90403957839971b
khoanguyen19 <61375967+khoanguyen19@users.noreply.github.com>
2021-08-05 21:12:19 +0700

    add prettier lint-staged husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,23 +2,21 @@ const newsRouter = require('./news.js');
 const siteRouter = require('./site.js');
 
 function route(app) {
-
     app.use('/news', newsRouter);
-   
-    app.use('/search', siteRouter);
-    
+
     app.use('/', siteRouter);
-    
-    
+
     // app.get('/news', (req, res) => {
     //     res.render('news');
     // })
 
-    
+    // app.get('/', (req, res) => {
+    //     res.render('home');
+    // })
+
     // app.get('/search', (req, res) => {
     //     res.render('search');
     // })
-  
 }
 
 module.exports = route;
